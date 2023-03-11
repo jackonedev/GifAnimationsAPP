9e3a8cbbbc79947c4e27324fc5880adfe797f91d
unknown <jackone.action.software@gmail.com>
2023-03-11 17:37:17 -0300

    YA ESTOY CON LA GRILLA HORIZONTAL Y AJUSTANDO LOS TICKS DE X

--- python_package/data_feed.py
+++ python_package/data_feed.py
@@ -42,7 +42,7 @@ def build_frames(data: pd.DataFrame, label: str, window:int=0) -> pd.DataFrame:
     global y_lim_max, y_lim_min
     y_lim_max = data[label].max()
     y_lim_min = data[label].min()
-    repeat=True
+    repeat=True##TODO: El parámetro repeat está en plot_data(), la cosa es que por ahora puedo diferenciar entre dos estilos diferentes de construir el deque
     if not window:
         window = len(data)
         repeat = False
@@ -58,37 +58,32 @@ def build_frames(data: pd.DataFrame, label: str, window:int=0) -> pd.DataFrame:
     row_keys = [f'{i}' for i in range(window)]
     return pd.DataFrame(lista_frames, columns=row_keys)
 
-
+### Donde veas 3 "#" el mensaje es para tí Chat GPT·
 
 def plot_data(df, fps=24, repeat=True, cache=False):
     fig, ax = plt.subplots()
     line, = ax.plot([], [])
     interval = 1000/fps
-    ax.set_xlim(0, 5)
+    ax.set_xlim(0, 5) ### este ax.set_xlim(0, 5) no se vé porque el objeto ax se actualiza en la función update
     
-    
-    # def update(i):
-    #     ax.clear()
-    #     line, = ax.plot(df.iloc[i])#, c='steelblue', linewidth=2.5,markeredgecolor='black', markeredgewidth=1.2, label='y = x^2')
-        
 
-    #     return line, 
-
+    plt.rcParams[]
 
     def update(i):
-        ax.clear()
-        ax.set_title('Hola mundo!')
-        ax.set_xlabel('Tiempo')
-        ax.set_ylabel('Valor')
-        ax.set_xlim(0, len(df.columns) - 1)
-        ax.set_ylim(y_lim_min, y_lim_max)
-        ax.set_xticks(range(0, len(df.columns), 5))
-        ax.set_xticklabels([str(x) for x in range(0, len(df.columns), 5)])
+        ax.clear() ### Debajo de este ax.clear() se actualiza el objeto ax y tiene mucha carga de trabajo
+        # ax.set_title('Hola mundo!')
+        # ax.set_xlabel('Tiempo')
+        # ax.set_ylabel('Valor')
+        # ax.set_xlim(0, len(df.columns) - 1)
+        ax.set_ylim(y_lim_min, y_lim_max)#TODO: esta variable global no se actualiza
+        # ax.set_xticks(range(0, len(df.columns), 5))
+        # ax.set_xticklabels([str(x) for x in range(0, len(df.columns), 5)])
         line, = ax.plot(df.iloc[i])
         return line, 
 
     return FuncAnimation(fig, update, frames=len(df), interval=interval, blit=True, repeat=repeat, cache_frame_data=cache)
 
+### Que estilización se le puede hacer al gráfico desde la librería matplo
 
 def main():
     file_name = "data-processed.pkl"
@@ -113,6 +108,49 @@ def main():
 #    print (len(frames['flow_rate']))
 
     ani = plot_data(frames['flow_rate'])
+
+
+
+def apply_dark_mode():
+    """https://matplotlib.org/3.5.0/tutorials/introductory/customizing.html
+    https://matplotlib.org/3.1.1/tutorials/colors/colors.html"""
+
+    import matplotlib.pyplot as plt
+    from matplotlib import cycler
+
+    # import matplotlib as mpl
+
+    colors = cycler(
+        "color", ["#669FEE", "#66EE91", "#9988DD", "#EECC55", "#88BB44", "#FFBBBB"]
+    )
+
+    plt.rc("figure", facecolor="#313233")
+    plt.rc(
+        "axes",
+        facecolor="#313233",
+        edgecolor="none",
+        axisbelow=True,
+        grid=True,
+        prop_cycle=colors,
+        labelcolor="0.81",
+    )
+    plt.rc("grid", color="474A4A", linestyle="--", alpha=0.7)
+    plt.rc("    ", color="0.81", labelsize=12)
+    plt.rc("ytick", direction="out", color="0.81", labelsize=12)
+    plt.rc("legend", facecolor="#313233", edgecolor="#313233")
+    plt.rc("text", color="#C9C9C9")
+    
+    plt.rcParams["axes.grid.which"] = "major" 
+    plt.rcParams["axes.grid.axis"] = "x"
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
